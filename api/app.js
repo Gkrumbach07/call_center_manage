ddd709ad926547fd1dc19038471443fc036f0a54
Gkrumbach07 <gkrumbac@redhat.com>
2021-01-18 20:29:47 -0600

    typo error

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ var kafka = require('kafka-node'),
      Consumer = kafka.Consumer,
      client = new kafka.KafkaClient(
        {
-         kafkaHost: `${KAFKA_HOST}:9092"`,
+         kafkaHost: `${KAFKA_HOST}:9092`,
          ssl: true
        }
      ),
