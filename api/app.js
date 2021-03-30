8bb2d88c3c43af4e1f9dde477ffc94869ebfca5f
Gage Krumbach <gkrumbach@gmail.com>
2021-03-30 09:43:12 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const fs = require('fs')
 
 const port = process.env.PORT || 8080;
 const host = process.env.IP || '0.0.0.0';
