513f67c9b0b84dac1fb43aa8569f01d159164608
Gage Krumbach <gkrumbach@gmail.com>
2021-05-27 09:24:29 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -37,7 +37,7 @@ const kafka = new Kafka({
   },
 })
 
-const consumer = kafka.consumer({ groupId: "test-group" })
+const consumer = kafka.consumer()
 
 const run = async () => {
   await consumer.connect()
